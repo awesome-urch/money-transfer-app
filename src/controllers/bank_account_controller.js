edf0cb6912c632dc9af88dbac9f58ae709b753e7
awesome-urch <nnabugwu.michael@gmail.com>
2023-04-27 17:56:39 +0100

    modified method for saving source and destination account

--- src/controllers/bank_account_controller.js
+++ src/controllers/bank_account_controller.js
@@ -5,6 +5,13 @@ const BaseController = require("./base_controller");
 class BankAccountController extends BaseController {
 
   async saveSourceAndDestination(props){
+    const checkBankAccount = await new BankAccount().findOne({
+      bank: props.bank,
+      account_number: props.account_number
+    });
+    if(checkBankAccount){
+      return checkBankAccount;
+    }
     const bankAccount = await new BankAccount().create(props);
     const getBankAccount = await new BankAccount().findOne({id:bankAccount[0]});
     return getBankAccount;
